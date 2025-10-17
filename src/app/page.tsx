571d0ed1713fc5864b26b1aeaf5982f009453ccb
Bayathy <misosoup0321@icloud.com>
2025-10-17 21:43:23 +0900

    feat: Tiptapエディタコンポーネントを実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@heroui/react";
+import Editor from "@/features/editor/components/editor";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Button>Click me</Button>
+      <Button color="primary" onPress={() => console.log("clicked")}>
+        Click me
+      </Button>
+      <Editor />
     </div>
   );
 }
